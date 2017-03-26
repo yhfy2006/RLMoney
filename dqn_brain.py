bc9954aa6de93a4b760165c47b13a0bead531888
chhe <chhe@ebay.com>
2017-03-26 18:49:42 -0400

    initiated den brain (not runable)

---
+++ dqn_brain.py
@@ -0,0 +1,144 @@
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Input, Dense, LSTM, Dropout
+from keras.models import Model
+from keras import backend as K
+
+class DeepQNetwork:
+    def __init__(
+            self,
+            n_actions,
+            n_features,
+            learning_rate=0.05,
+            reward_decay=0.9,
+            e_greedy=0.9,
+            replace_target_iter=500,
+            memory_size=500,
+            batch_size=32,
+            rnn_train_length = 30,
+            e_greedy_increment=None,
+            output_graph=False,
+    ):
+        self.n_actions = n_actions
+        self.n_features = n_features
+        self.lr = learning_rate
+        self.gamma = reward_decay
+        self.epsilon_max = e_greedy
+        self.replace_target_iter = replace_target_iter
+        self.memory_size = memory_size
+        self.batch_size = batch_size
+        self.rnn_train_length = rnn_train_length
+        self.epsilon_increment = e_greedy_increment
+        self.epsilon = 0 if e_greedy_increment is not None else self.epsilon_max
+
+        # total learning step
+        self.learn_step_counter = 0
+
+        # initialize zero memory [s, a, r, s_]
+        self.memory = pd.DataFrame(np.zeros((self.memory_size, n_features*2+2)))
+
+        # consist of [target_net, evaluate_net]
+        self._build_net()
+
+        self.cost_his = []
+
+    def my_init(self,shape, name=None):
+        value = np.random.random(shape)
+        return K.variable(value, name=name)
+
+    def _build_keras_net(self):
+        model = Sequential()
+        model.add(LSTM(30, input_dim=1))
+        # model.add(Dropout(0.5))
+        # model.add(LSTM(30, batch_input_shape=(20,3,1),stateful=True))
+        # model.add(Dropout(0.4))
+        # model.add(Dense(self.n_actions,init="normal"))
+        model.compile(optimizer='adam', loss='mse',metrics=['accuracy'])
+        print(model.summary())
+        return model
+
+        # input = Input(shape=(20,self.n_features))
+        # x = LSTM(128, batch_input_shape=(20,self.rnn_train_length, self.n_features),stateful=True,return_sequences=True)(input)
+        # x = Dropout(0.5)(x)
+        # x = LSTM(128, batch_input_shape=(20,self.rnn_train_length, self.n_features),stateful=True)(x)
+        # x = Dropout(0.4)(x)
+        # output = Dense(self.n_actions,init='normal')(x)
+        # md = Model(input = input, output=output)
+        # md.compile(optimizer='adam', loss='mse',metrics=['accuracy'])
+        # print(md.summary())
+        # return md
+
+    def _build_net(self):
+        # ------------------ build evaluate_net ------------------
+        self.evaluate_net = self._build_keras_net()
+        self.target_net = self._build_keras_net()
+
+
+    def store_transition(self, s, a, r, s_):
+        if not hasattr(self, 'memory_counter'):
+            self.memory_counter = 0
+
+        transition = np.hstack((s, [a, r], s_))
+        # replace the old memory with new memory
+        index = self.memory_counter % self.memory_size
+        self.memory.iloc[index, :] = transition
+        self.memory_counter += 1
+
+    def choose_action(self, observation):
+        # to have batch dimension when feed into tf placeholder
+        observation = observation[np.newaxis, :]
+        if np.random.uniform() < self.epsilon:
+            # forward feed the observation and get q value for every actions
+            actions_value =  self.evaluate_net.predict(observation)
+            action = np.argmax(actions_value)
+        else:
+            action = np.random.randint(0, self.n_actions)
+        return action
+
+    def _replace_target_params(self):
+        self.evaluate_net.save_weights("tempWeights.h5")
+        self.target_net.load_weights("tempWeights.h5")
+
+    def learn(self):
+        # check to replace target parameters
+        if self.learn_step_counter % self.replace_target_iter == 0:
+            self._replace_target_params()
+            print('\ntarget_params_replaced\n')
+
+        # sample batch memory from all memory
+        batch_memory = self.memory.sample(self.batch_size) \
+            if self.memory_counter > self.memory_size \
+            else self.memory.iloc[:self.memory_counter].sample(self.batch_size, replace=True)
+
+        self.s = batch_memory.iloc[:, :self.n_features].values
+        self.s_ = batch_memory.iloc[:, -self.n_features:].values
+
+        q_next = self.target_net.predict(self.s_)
+        q_eval = self.evaluate_net.predict(self.s)
+
+        q_target = q_eval.copy()
+        q_target[np.arange(self.batch_size, dtype=np.int32), batch_memory.iloc[:, self.n_features].astype(int)] = \
+            batch_memory.iloc[:, self.n_features+1] + self.gamma * np.max(q_next, axis=1)
+
+        # train eval network
+        history = self.evaluate_net.fit(self.s,q_target,
+                nb_epoch=1,
+                batch_size=50,
+                shuffle=False,
+                verbose=0)
+
+        self.cost_his.append(history.history['loss'])
+
+        # increasing epsilon
+        self.epsilon = self.epsilon + self.epsilon_increment if self.epsilon < self.epsilon_max else self.epsilon_max
+        #print(self.epsilon)
+        self.learn_step_counter += 1
+
+
+    def plot_cost(self):
+        import matplotlib.pyplot as plt
+        plt.plot(np.arange(len(self.cost_his)), self.cost_his)
+        plt.show()
---
+++ dqn_run.py
@@ -0,0 +1,55 @@
+__author__ = 'chhe'
+
+from game import Game
+from dqn_brain import DeepQNetwork
+
+
+def run_network():
+    step = 0
+    gameRun = 0
+    for episode in range(500000):
+        gameRun += 1
+        observation = env.reset()
+
+        while True:
+
+            action = RL.choose_action(observation)
+
+            observation_,reward,done,info = env.step(action)
+
+            RL.store_transition(observation,action,reward,observation_)
+
+            if(step > 300) and (step % 5 == 0):
+                RL.learn()
+
+            observation = observation_
+
+            if done:
+                break
+
+            step += 1
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    env = Game(250, 100, 5, 0.3)
+
+    n_actions = 3
+    n_features =4
+    RL = DeepQNetwork(n_actions, n_features,
+                      learning_rate=0.03,
+                      reward_decay=0.7,
+                      e_greedy=0.9,
+                      replace_target_iter=200,
+                      memory_size=2000,
+                      e_greedy_increment = 0.001
+                      # output_graph=True
+                      )
+    run_network()
