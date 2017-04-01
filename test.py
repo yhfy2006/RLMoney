5fd1c6c1677704b368338ab0a92ae4bccbe1dfc4
chhe <chhe@ebay.com>
2017-04-01 18:16:53 -0400

    cleaned up.

--- test.py
+++ test.py
@@ -1,13 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.axis([0, 10, 0, 1])
-plt.ion()
+values = []
+lines  = []
+with open('netValue.txt') as fp:
+    logLines = 0
+    for line in fp:
+        lines.append(logLines)
+        logLines += 1
+        value = float(line)
+        values.append(value)
+        #print(value)
 
-for i in range(10):
-    y = np.random.random()
-    plt.scatter(i, y)
-    plt.pause(0.05)
+print(sorted(values, key=int)[-10:])
+print(max(values))
 
-while True:
-    plt.pause(0.05)
+#plt.plot(values)
+plt.plot(lines,values)
+plt.axis([0, len(lines), 100, 200000])
+plt.show()
+
+
+values = []
+lines  = []
+with open('log.txt') as fp:
+    logLines = 0
+    for line in fp:
+        lines.append(logLines)
+        logLines += 1
+        value = line.split(":")[-1]
+        value = float(value)
+        values.append(value)
+        #print(value)
+
+print(sorted(values, key=int)[-10:])
+print(max(values))
+
